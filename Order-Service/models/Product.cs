6241b6aaa1b85fea5bcc6ec2bd07208d7d5d484e
abbas alabbas <abbas0alabbas@gmail.com>
2025-12-24 20:23:55 +0300

    Fix producy serialization problem in case non integer

--- Order-Service/models/Product.cs
+++ Order-Service/models/Product.cs
@@ -4,7 +4,7 @@ namespace OrderService.Models;
 
 public class Product
 {
-    public int Id { get; set; }
+    public string Id { get; set; }
     public string Name { get; set; }
     public decimal Price { get; set; }
 }
